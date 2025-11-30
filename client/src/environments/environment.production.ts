7618f4d6267bb8d2b7092ddca205acc33b323a4f
noviKorisnik <novikorisnik@gmail.com>
2025-11-30 20:24:42 +0100

    feat: add production environment configuration for API and WebSocket URLs

---
+++ client/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+const serverUrl = 'https://abyssal-g4dtbec5a3e7gzfx.germanywestcentral-01.azurewebsites.net/'
+
+export const environment = {
+  apiBaseUrl: `${serverUrl}/api`,
+  wsBaseUrl: `${serverUrl}/ws`,
+  production: true,
+};
